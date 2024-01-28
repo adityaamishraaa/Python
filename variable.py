53178518b6e62a666486513e720d5a8390b041b6
adityaamishraaa <118230920+adityaamishraaa@users.noreply.github.com>
2024-01-28 11:27:59 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,22 @@
+first_name = "Aditya"
+last_name = "Mishra"
+full_name = first_name +" "+ last_name
+
+print(full_name)
+print(type(full_name))
+
+age = 20
+age += 2
+print ("My age is:"+ str(age))
+print(type(age))
+
+height = 177.5
+print(height)
+print(type(height))
+
+human = False
+print("are you a human: "+ str(human))
+print (human)
+
+#variables are of four type :
+# string, int, float, boolean
